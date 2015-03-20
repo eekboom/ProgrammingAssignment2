6fbf7d99fd6fa79bbfea486a6def2f5f09bc3477
Stephen Friedrich <stephen.friedrich@gateprotect.com>
2015-03-20 17:05:41 +0100

    Caching Feature for Matrix Inversion
    Initial solution of the Programming Assignment 2
    of Coursera's R Programming Course

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,69 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Offers a feature to cache a matrix's inverse matrix to avoid potentially
+## costly recalculations.
+##
+## This is the Programming Assignment 2 for Coursera's "R Programming" course:
+## Goal: Use lexical scoping to create a closure where values can be cached.
 
-## Write a short comment describing this function
+## Function comments are in roxygen format, see
+## http://kbroman.org/pkg_primer/pages/docs.html
+## Code uses indentation of 4 spaces as a compromise between readability and
+## space efficiency.
 
+#' Creates a cache that stores a matrix and can cache its inverse matrix
+#' @param x initial value of the matrix
+#' @export
 makeCacheMatrix <- function(x = matrix()) {
+    inverse <- NULL
 
+    #' Set a new matrix (will clear the cached inverse matrix)
+    #' @param y new value
+    set <- function(y) {
+        x <<- y
+        inverse <<- NULL
+    }
+    #' Get the matrix
+    get <- function() {
+        x
+    }
+
+    #' Stores the matrix's inverse matrix for future retrieval
+    setInverse <- function(inverse) {
+        # Interestingly the following assignment works (though it is not
+        # recommended).
+        # I found an eplanation in the "R Language Definition",
+        # section 3.4.4 "Subset assignment".
+        # Right hand side is evaluated in current environment (parameter),
+        # left hand side is evaluated in parent environment and up (closure).
+        inverse <<- inverse
+    }
+
+    #' Returns the cached inverse (or NULL if no inverse has been stored since
+    #' the matrix was set)
+    getInverse <- function() {
+        inverse
+    }
+
+    list(set = set, get = get, getInverse = getInverse, setInverse = setInverse)
 }
 
 
-## Write a short comment describing this function
-
+#' Takes a cache matrix (as returned from makeCacheMatrix()) and returns its
+#' (potentially cached) inverse matrix.
+#'
+#' @param x a cache matrix object as returned from makeCacheMatrix()
+#' @return the inverse
+#' @export
+#' @examples
+#' m <- makeCacheMatrix(matrix(1:4, 2, 2))
+#' inverse <- cacheSolve(m)
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inverse <- x$getInverse()
+
+    if(is.null(inverse)) {
+        # not yet cached, calculate and put into cache
+        inverse <- solve(x$get())
+        x$setInverse(inverse)
+    }
+
+    inverse
 }
